3509187b977e9ac659e930b89294d334ba3b20a3
adrianarmm <areyemor@myuax.com>
2023-12-12 19:22:13 +0100

    53

--- 3.cpp
+++ 3.cpp
@@ -22,3 +22,9 @@ int main() {
     strcpy(estudiante1.nombre, "Adriana Reyes");
     estudiante1.edad = 18;
     estudiante1.promedio = 9;
+
+    Estudiante estudiante2 = copiarEstudiante(estudiante1);
+
+   printf("Nombre: %s\n", estudiante2.nombre);
+   printf("Edad: %d\n", estudiante2.edad);
+   printf("Promedio: %.2f\n", estudiante2.promedio);
